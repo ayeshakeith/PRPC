a93c9aff6b3d0ce890b04adb3e128507bec4d34a
Ayesha Keith Santos <ayeshakeithsantos@gmail.com>
2022-04-12 20:36:11 +0800

    wip student-side
    - added navbar
    - started working on dashboard header

--- Student/student_dashboard.php
+++ Student/student_dashboard.php
@@ -1,1 +1,21 @@
-<?php echo "Welcome, Student."; ?>
+<html>
+    <head>
+        <title>Student Dashboard</title>
+
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+            <?php include('student_navbar.php'); ?>
+
+            <iframe style="width: 100%; height: 80%;" src="https://www.youtube.com/embed/4Q1FzhMY8FQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            
+            
+    </body>
+</html>
---
+++ Student/student_navbar.php
@@ -0,0 +1,27 @@
+<style>
+            .topnav-centered a {
+            float: none;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            color: #003d60;
+            }
+</style>
+
+<nav class="navbar navbar-inverse" style="border-bottom: 0px; margin-bottom:0px;">
+    <div class="container-fluid" style="background-color: #f1ece9;">
+        <ul class="nav navbar-nav">
+        <li><a href="#" style="color: #003d60;">Home</a></li>
+        <li><a href="#" style="color: #003d60;">Courses</a></li>
+        <li><a href="#" style="color: #003d60;">Learning Tools</a></li>
+        </ul>
+        <div class="topnav-centered">
+            <a href="#">OnlineSesh</a>
+        </div>
+        <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-out"></span></a></li>
+        </ul>
+        
+    </div>
+</nav>
