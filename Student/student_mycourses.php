eabcf308424e847f5d5de47adb2ad9a792c3c637
Gabriela Andes <gabriela.andes05@gmail.com>
2022-04-13 18:37:26 +0800

    Updated
    Made the card horizontal

--- Student/student_mycourses.php
+++ Student/student_mycourses.php
@@ -45,23 +45,26 @@
             
             .row-courses{
                 margin: 0 auto;
-                padding: 130px;
-                height: 135vh;
+                padding: 140px;
+                height: 150vh;
                 background-color: #fccb96;
             }
 
-            .card-body{
-                padding: 0px;
-                background-color: #fccb96; 
-                margin-bottom: 70px;
-                margin-left: 67px; 
-                height: 49vh;
+            .card{
+                margin-bottom: 30px;
             }
 
+            .card-image{
+                padding: 0px;
+                margin: 0px;
+                left: 0;
+                height: 157px;
+                width: 100%;
+            }
 
             .footer-container{
                 background-color: #130d4c;
-                margin-top: 20%;
+                margin-top: 30%;
                 width: 100%;
                 height: 5vh;
                 color: white;
@@ -86,101 +89,83 @@
 
             <div class="container-courses" style="font-family: Poppins;">
             <div class="row-courses">
-                <div class="card-body col-md-3">
-                    <div class="panel panel-default">
-                    <div class="panel-heading" style="padding:0px; margin:0px;">
-                        <img src="https://images.pexels.com/photos/247929/pexels-photo-247929.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="books" height="200px" width="100%">
-                    </div>
-                    <div class="panel-body" style="border: none;">
-                        <h4> Subject Name </h4>
-                        <br> 
-                        <p> Date and Time </p>
-                        <p> Prof Name </p>
-                        <br>
-                        <button class="btn btn-primary" type="submit">Button</button>
-                    </div>
+
+                <div class="card">
+                    <div class="row">
+                        <div class="col-md-3" style="background-color: white; padding: 0px">
+                            <img src="https://images.pexels.com/photos/247929/pexels-photo-247929.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-image" alt="books">
+                        </div>
+                        <div class="col-md-9" style="background-color: white; height:157px;">
+                            <a href="#"> <h2 class="card-title mt-3"> Computer Programming 1 </h2> </a>
+                            <p class="muted-text"> CS126-8 </p>
+                            <p> This course focuses on the logic formulation in solving problem using flowcharting and pseudocodes. </p>
+                            <p style="margin-top: -5px"> Instructor: Kamisato Ayato </p>
+                        </div>
                     </div>
                 </div>
 
-                <div class="card-body col-md-3">
-                    <div class="panel panel-default">
-                    <div class="panel-heading" style="padding:0px; margin:0px;">
-                        <img src="https://images.pexels.com/photos/247929/pexels-photo-247929.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="books" height="200px" width="100%">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-md-3" style="background-color: white; padding: 0px;">
+                            <img src="https://images.pexels.com/photos/92866/pexels-photo-92866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-image" alt="books">
+                        </div>
+                        <div class="col-md-9" style="background-color: white; height:157px;">
+                            <a href="#"> <h2 class="card-title mt-3"> Computer Programming Laboratory 1 </h2> </a>
+                            <p class="muted-text"> CS126-8L </p>
+                            <p> The course focuses on hands-on activities on topics such as: data types, control structures, and functions. </p>
+                            <p style="margin-top: -5px"> Instructor: Kamisato Ayato </p>
+                            <!-- <button class="btn btn-primary" type="submit">Button</button> -->
+                            <br>
+                        </div>
                     </div>
-                    <div class="panel-body" style="border: none;">
-                        <h4> Subject Name </h4>
-                        <br> 
-                        <p> Date and Time </p>
-                        <p> Prof Name </p>
-                        <br>
-                        <button class="btn btn-primary" type="submit">Button</button>
-                    </div>
-                    </div>
-                 </div>
+                </div>
 
-                 <div class="card-body col-md-3">
-                    <div class="panel panel-default">
-                    <div class="panel-heading" style="padding:0px; margin:0px;">
-                        <img src="https://images.pexels.com/photos/247929/pexels-photo-247929.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="books" height="200px" width="100%">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-md-3" style="background-color: white; padding: 0px;">
+                            <img src="https://images.pexels.com/photos/247929/pexels-photo-247929.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-image" alt="books">
+                        </div>
+                        <div class="col-md-9" style="background-color: white; height:157px;">
+                            <a href="#"> <h2 class="card-title mt-3"> Understanding The Self </h2> </a>
+                            <!-- <p class="muted-text"> GED101 </p> -->
+                            <p> The course deals with the nature of identity, as well as the factors and forces that affects the development of our personal identity. </p>
+                            <p> Instructor: Thoma </p>
+                            <!-- <button class="btn btn-primary" type="submit">Button</button> -->
+                            <br>
+                        </div>
                     </div>
-                    <div class="panel-body" style="border: none;">
-                        <h4> Subject Name </h4>
-                        <br> 
-                        <p> Date and Time </p>
-                        <p> Prof Name </p>
-                        <br>
-                        <button class="btn btn-primary" type="submit">Button</button>
-                    </div>
-                    </div>
-                 </div>
+                </div>
 
-                 <div class="card-body col-md-3">
-                    <div class="panel panel-default">
-                    <div class="panel-heading" style="padding:0px; margin:0px;">
-                        <img src="https://images.pexels.com/photos/247929/pexels-photo-247929.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="books" height="200px" width="100%">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-md-3" style="background-color: white; padding: 0px;">
+                            <img src="https://images.pexels.com/photos/92866/pexels-photo-92866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-image" alt="books">
+                        </div>
+                        <div class="col-md-9" style="background-color: white; height:157px;">
+                            <a href="#"> <h2 class="card-title mt-3"> Introduction to Computer Science </h2> </a>
+                            <p> This course provides an overview of the Computing Industry and Computing profession, with Research and practical applications </p>
+                            <p> Instructor: Tartaglia </p>
+                            <!-- <button class="btn btn-primary" type="submit">Button</button> -->
+                            <br>
+                        </div>
                     </div>
-                    <div class="panel-body" style="border: none;">
-                        <h4> Subject Name </h4>
-                        <br> 
-                        <p> Date and Time </p>
-                        <p> Prof Name </p>
-                        <br>
-                        <button class="btn btn-primary" type="submit">Button</button>
-                    </div>
-                    </div>
-                 </div>
+                </div>
 
-                 <div class="card-body col-md-3">
-                    <div class="panel panel-default">
-                    <div class="panel-heading" style="padding:0px; margin:0px;">
-                        <img src="https://images.pexels.com/photos/247929/pexels-photo-247929.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="books" height="200px" width="100%">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-md-3" style="background-color: white; padding: 0px">
+                            <img src="https://images.pexels.com/photos/247929/pexels-photo-247929.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-image" alt="books">
+                        </div>
+                        <div class="col-md-9" style="background-color: white; height:157px;">
+                            <h2 class="card-title mt-3"> Subject Course </h2>
+                            <p> Date and Time </p>
+                            <p> Professor Name </p>
+                            <button class="btn btn-primary" type="submit">Button</button>
+                            <br>
+                        </div>
                     </div>
-                    <div class="panel-body" style="border: none;">
-                        <h4> Subject Name </h4>
-                        <br> 
-                        <p> Date and Time </p>
-                        <p> Prof Name </p>
-                        <br>
-                        <button class="btn btn-primary" type="submit">Button</button>
-                    </div>
-                    </div>
-                 </div>
+                </div>
 
-                 <div class="card-body col-md-3">
-                    <div class="panel panel-default">
-                    <div class="panel-heading" style="padding:0px; margin:0px;">
-                        <img src="https://images.pexels.com/photos/247929/pexels-photo-247929.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="books" height="200px" width="100%">
-                    </div>
-                    <div class="panel-body" style="border: none;">
-                        <h4> Subject Name </h4>
-                        <br> 
-                        <p> Date and Time </p>
-                        <p> Prof Name </p>
-                        <br>
-                        <button class="btn btn-primary" type="submit">Button</button>
-                    </div>
-                    </div>
-                 </div> 
             </div>
             </div>
 
