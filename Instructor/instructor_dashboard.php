8c58a70d6f49683e0fe2361b776532e9ce2100c3
Ayesha Keith Santos <ayeshakeithsantos@gmail.com>
2022-04-13 21:09:41 +0800

    dashboard quick fix

--- Instructor/instructor_dashboard.php
+++ Instructor/instructor_dashboard.php
@@ -25,8 +25,6 @@
                 <h2> Online Classes From Everywhere </h2> <br><br> 
                 <button type="button" class="btn btn-default btn-sm"> <span class="glyphicon glyphicon-play"></span> Play Video </button> </div>
             </div>
-
-            <?php echo  $_SESSION['name']; ?>
             
             <div class="grid-container">
                 <div class="grid-item"><iframe style="width: 100%;" src="https://www.youtube.com/embed/BnxfuvRHKDk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
