1d48d938b5c9d20982860f43f8e53190c8b54630
rbellazar <87217997+rbellazar@users.noreply.github.com>
2022-04-14 07:39:04 +0800

    instructor side

---
+++ Instructor/instructor_activities_style.php
@@ -0,0 +1,60 @@
+<style>
+			.side-navbar{
+                background-color: #130d4c;
+                width: 100%;
+                height: 5vh;
+                color: white;
+            }
+			
+			.header-container{
+                background-color: #130d4c;
+                margin-top: 0;
+                width: 100%;
+                height: 10vh;
+                color: white;
+				padding-top:17px;
+				padding-left:45vw;
+            }
+			
+			.body-container{
+                background-color: white;
+				border: 3px solid #130d4c;
+                width: 82%;
+                height: 75vh;
+                color: white;
+				margin-left:17vw;
+				margin-top:1vw;
+				padding-top:1vw;
+            }
+			
+			.body1-container{
+                background-color: white;
+				border: 3px solid #130d4c;
+                width: 82%;
+                height: 75vh;
+                color: white;
+				margin-left:1vw;
+				margin-top:1vw;
+				padding-top:1vw;
+            }
+			
+			.shesh{
+				font-size:28px; 
+				background-color:#130d4c; 
+				margin-left:-15vw;
+				margin-bottom: 18px; 
+				margin-top: 20px;
+				color:#FCCB96; 
+				padding:12; 
+				border-radius:24px;
+			}
+			
+            .footer-container{
+                background-color: #130d4c;
+                margin-top: 20;
+                width: 100%;
+                height: 5vh;
+                color: white;
+            }
+
+</style>
