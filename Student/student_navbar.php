8bff9b07c134f7fc6ad683cce02309465786cbf6
Ayesha Keith Santos <ayeshakeithsantos@gmail.com>
2022-04-13 11:23:10 +0800

    linked homepage

--- Student/student_navbar.php
+++ Student/student_navbar.php
@@ -19,7 +19,7 @@
 <nav class="navbar navbar-inverse navbar-sticky" style="border-bottom: 0px; margin-bottom:0px;">
     <div class="container-fluid" style="background-color: #f1ece9;">
         <ul class="nav navbar-nav">
-        <li><a href="#" style="color: #003d60;">Home</a></li>
+        <li><a href="student_dashboard.php" style="color: #003d60;">Home</a></li>
         <li><a href="student_mycourses.php" style="color: #003d60;">Courses</a></li>
         <li><a href="#" style="color: #003d60;">Learning Tools</a></li>
         </ul>
